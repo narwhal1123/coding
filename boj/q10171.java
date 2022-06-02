7d17c6565ee7de6766c8c7cce4ef79c48484cc43
YangHun Jung <narwhal1123@gmail.com>
2022-06-02 23:45:04 +0900

    basic q

---
+++ boj/q10171.java
@@ -0,0 +1,12 @@
+public class q10171 {
+    public static void main(String[] args) {
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
