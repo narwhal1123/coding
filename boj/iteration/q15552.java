a0ebb6e7906acdec11b15ee5745f46a1e3825a2a
YangHun Jung <narwhal1123@gmail.com>
2022-06-05 01:18:08 +0900

    iteration

---
+++ boj/iteration/q15552.java
@@ -0,0 +1,19 @@
+package iteration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q15552 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int test = Integer.parseInt(bufferedReader.readLine());
+            for (int i = 0; i < test; i++) {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
