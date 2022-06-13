e17b1b7305165f54efe3bfc325e68fca1fb6a0e2
YangHun Jung <narwhal1123@gmail.com>
2022-06-13 20:55:16 +0900

    basicMath

--- boj/basicMath/q1712.java
+++ boj/basicMath/q1712.java
@@ -1,7 +1,18 @@
 package basicMath;
 
+import java.util.Scanner;
+
 public class q1712 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int fix = scanner.nextInt();
+        int var = scanner.nextInt();
+        int sell = scanner.nextInt();
 
+        if (sell - var <= 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(fix / (sell - var) + 1);
+        }
     }
 }
