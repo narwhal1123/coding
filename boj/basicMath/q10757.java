3ec3e6455a0c98cb745d5cd8a1b1b19dbae4fa58
YangHun Jung <narwhal1123@gmail.com>
2022-06-16 23:25:04 +0900

    basicMath

---
+++ boj/basicMath/q10757.java
@@ -0,0 +1,19 @@
+package basicMath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class q10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+        BigInteger b1 = new BigInteger(stringTokenizer.nextToken());
+        BigInteger b2 = new BigInteger(stringTokenizer.nextToken());
+
+        System.out.println(b1.add(b2));
+    }
+}
