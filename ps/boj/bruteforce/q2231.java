1b66fd1f2a8978691f815cb8bc217b4f0e17d938
YangHun Jung <narwhal1123@gmail.com>
2022-07-04 23:01:25 +0900

    bruteforce

--- ps/boj/bruteforce/q2231.java
+++ ps/boj/bruteforce/q2231.java
@@ -1,7 +1,24 @@
 package boj.bruteforce;
 
+import java.util.Scanner;
+
 public class q2231 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
 
+        for (int i = 1; i < input; i++) {
+            int temp = i;
+            int sum = temp;
+            while(temp > 0){
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (sum == input) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
     }
 }
