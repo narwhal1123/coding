a3eb53ac6fc5d47ba033de50843e43ecbe1b51ee
jyh <narwhal1123@gmail.com>
2022-09-05 17:44:04 +0900

    stack

---
+++ ps/boj/stack/q17298.java
@@ -0,0 +1,69 @@
+package boj.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제
+크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다.
+Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+
+예를 들어, A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다.
+A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+
+출력
+총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
+
+
+ */
+public class q17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bufferedReader.readLine());
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        int[] input = new int[num];
+        for (int i = 0; i < num; i++) {
+            input[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        int[] stack = new int[num];
+        int[] answer = new int[num];
+        int endPointer = 0;
+        for (int i = num - 1; i >= 0; i--) {
+            if (endPointer == 0) { // 스택에 값이 없다면 -1 출력하고 현재 값 넣기
+                answer[i] = -1;
+                stack[endPointer++] = input[i];
+            } else if (input[i] >= stack[endPointer - 1]) { // stack 맨 위의 값보다 크다면 스택을 내려가면서 값 비교
+                while (input[i] > stack[endPointer - 1]) {
+                    endPointer--;
+                    if (endPointer == 0) {
+                        break;
+                    }
+                }
+                if (endPointer == 0) {
+                    answer[i] = -1;
+                    stack[endPointer++] = input[i];
+                } else {
+                    answer[i] = stack[endPointer - 1];
+                    stack[endPointer++] = input[i];
+                }
+            } else if (input[i] < stack[endPointer - 1]) { // stack맨 위의 값보다 작다면 맨 위 값 출력 후 현재 값 스택에 넣어둠
+                answer[i] = stack[endPointer - 1];
+                stack[endPointer++] = input[i];
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int a : answer) {
+            stringBuilder.append(a + " ");
+        }
+
+        System.out.println(stringBuilder);
+    }
+}
--- ps/boj/stack/q1874.java
+++ ps/boj/stack/q1874.java
@@ -1,6 +1,14 @@
 package boj.stack;
 
 /*
+스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다.
+스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+
+1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다.
+이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자.
+임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다.
+이를 계산하는 프로그램을 작성하라.
+
 입력
 첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다. 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다.
 물론 같은 정수가 두 번 나오는 일은 없다.
@@ -18,9 +26,35 @@ public class q1874 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(bufferedReader.readLine());
 
+        int[] result = new int[num];
+        for (int i = 0; i < num; i++) {
+            result[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
         int[] stack = new int[num];
         int endPointer = 0;
+        int temp = 1;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            while (result[i] >= temp) { // push
+                stack[endPointer] = temp;
+                temp++;
+                stringBuilder.append("+" + '\n');
+                endPointer++;
+            }
+            // pop
+            if (result[i] == stack[endPointer - 1]) {
+                stringBuilder.append("-" + '\n');
+                endPointer--;
+            }
+        }
 
+
+        if (endPointer != 0) {
+            System.out.println("NO");
+        } else {
+            System.out.println(stringBuilder);
+        }
 
     }
 }
