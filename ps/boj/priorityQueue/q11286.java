b494e057113cd8eb6f9f89951c094ab5b336341b
jyh <narwhal1123@gmail.com>
2022-10-06 17:52:30 +0900

    priorityQueue

--- ps/boj/priorityQueue/q11286.java
+++ ps/boj/priorityQueue/q11286.java
@@ -1,4 +1,10 @@
 package boj.priorityQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
 /*
 문제
 절댓값 힙은 다음과 같은 연산을 지원하는 자료구조이다.
@@ -13,7 +19,7 @@ package boj.priorityQueue;
 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다.
 만약 x가 0이 아니라면 배열에 x라는 값을 넣는(추가하는) 연산이고,
 x가 0이라면 배열에서 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다.
-입력되는 정수는 -231보다 크고, 231보다 작다.
+입력되는 정수는 -2^31보다 크고, 2^31보다 작다.
 
 출력
 입력에서 0이 주어진 회수만큼 답을 출력한다.
@@ -51,7 +57,35 @@ x가 0이라면 배열에서 절댓값이 가장 작은 값을 출력하고 그 
 0
  */
 public class q11286 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bufferedReader.readLine());
 
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            int num1 = Math.abs(o1);
+            int num2 = Math.abs(o2);
+
+            if (num1 == num2) {
+                return o1 - o2;
+            } else {
+                return num1 - num2;
+            }
+        });
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            if (input == 0) {
+                if (priorityQueue.size() != 0) {
+                    stringBuilder.append(priorityQueue.poll()).append('\n');
+                } else {
+                    stringBuilder.append("0\n");
+                }
+            } else {
+                priorityQueue.offer(input);
+            }
+        }
+
+        System.out.println(stringBuilder);
     }
 }
